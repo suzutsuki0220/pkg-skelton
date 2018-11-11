02f1eafada502edf30a8f3ea8bd0a0a2e01db99f
suzutsuki0220 <suzutsuki0220@gmail.com>
2018-11-11 16:07:20 +0900

    test: add travis CI config

---
+++ test/datetime.test.js
@@ -0,0 +1,16 @@
+describe('datetime', () => {
+  let datetime
+  beforeEach(() => {
+    datetime = require('../datetime');
+  })
+
+  describe('isValidString()', () => {
+    test('isValidString', () => {
+      expect(datetime.isValidString('')).toBe(false)
+      expect(datetime.isValidString('2018-11-11 12:34:56.123')).toBe(true)
+      expect(datetime.isValidString('2018-11-11 24:60:60.123')).toBe(false)
+      expect(datetime.isValidString('2018-13-11 12:34:56.123')).toBe(false)
+      expect(datetime.isValidString('2018-11-32 12:34:56.123')).toBe(false)
+    })
+  })
+})
