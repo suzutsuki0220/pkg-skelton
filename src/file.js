b15ac28c5b179a11f4aa50cf23eb814415c66437
suzutsuki0220 <suzutsuki0220@gmail.com>
2019-02-18 06:21:53 +0000

    feat: add file <=> base64 convert

--- src/file.js
+++ src/file.js
@@ -1,4 +1,7 @@
-// Blobオブジェクトを使ってdataのダウンロード動作を行う
+/**
+ * Blobオブジェクトを使ってdataのダウンロード動作を行う
+ *  windowオブジェクトを使うためブラウザ専用
+**/
 module.exports.saveBlob = function(data, filename) {
     const blob = new Blob([data], {"type": "application/octet-stream"});
 
@@ -15,4 +18,38 @@ module.exports.saveBlob = function(data, filename) {
         a.click();
         document.body.removeChild(a);
     }
+};
+
+// available encoding
+//   utf8(default), ascii, utf16le, ucs2, base64, hex
+module.exports.load = function(filename, encoding) {
+    const fs = require('fs');
+
+    if (typeof encoding === undefined) {
+        return fs.readFileSync(filename, {encoding: 'buffer', flag: 'r'});
+    } else {
+        return fs.readFileSync(filename, {encoding: encoding, flag: 'r'});
+    }
+};
+
+module.exports.save = function(filename, data, encoding) {
+    const fs = require('fs');
+
+    if (typeof encoding === undefined) {
+        return fs.writeFileSync(filename, data, {encoding: 'buffer', flag: 'w'});
+    } else {
+        return fs.writeFileSync(filename, data, {encoding: encoding, flag: 'w'});
+    }
+};
+
+module.exports.FileToBase64 = function(filename) {
+    const Buffer = require('buffer').Buffer;
+    return Buffer.from(this.load(filename)).toString('base64');
+};
+
+module.exports.Base64ToFile = function(b64data, filename) {
+    const Buffer = require('buffer').Buffer;
+    const buf = Buffer.from(b64data, 'base64');
+
+    this.save(filename, buf);
 };
---
+++ test/file.test.js
@@ -0,0 +1,26 @@
+describe('file', () => {
+  let file;
+
+  beforeEach(() => {
+    file = require('../config').require.jsUtils.file;
+  });
+
+  describe('FileToBase64()', () => {
+    test('FileToBase64', () => {
+      expect(file.FileToBase64('./test/load_check.data')).toBe('YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXoK');
+    });
+  });
+
+  describe('Base64ToFile()', () => {
+    test('Base64ToFile', () => {
+      const fs = require('fs');
+      const path = require('path');
+      const os = require('os');
+
+      const tmp_file = path.join(os.tmpdir(), 'b64toFile.data');
+      file.Base64ToFile('YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXoK', tmp_file);
+      expect(file.load(tmp_file, 'ascii')).toBe('abcdefghijklmnopqrstuvwxyz\n');
+      expect(fs.unlinkSync(tmp_file)).toBe(undefined);
+    });
+  });
+});
