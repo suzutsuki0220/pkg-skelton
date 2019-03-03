e95b340a6e45cf30e8bf690ee81bfcc091ee3a87
suzutsuki0220 <suzutsuki0220@gmail.com>
2019-03-03 03:14:39 +0000

    test: add section split and encoded parameter check

--- test/url.test.js
+++ test/url.test.js
@@ -11,15 +11,36 @@ describe('url', () => {
       expect(url.getQueryInUrl("")).toBe("");
       expect(url.getQueryInUrl("http://192.168.0.241:82/stocker/head.html")).toBe("");
       expect(url.getQueryInUrl("http://192.168.0.241:82/stocker/head.html?")).toBe("");
+      expect(url.getQueryInUrl("http://192.168.0.241:82/stocker/head.html#")).toBe("");
       expect(url.getQueryInUrl("http://192.168.0.241:82/stocker/head.html? ")).toBe(" ");
     });
+    test('getRawParams', () => {
+      expect(url.getRawParams("http://192.168.0.241:82/stocker/head.html?dir=%E5%A4%89%E6%8F%9B&file=/MjAxOS0wMy0wMl8xMjUxMzY")).toEqual({dir:"%E5%A4%89%E6%8F%9B", file:"/MjAxOS0wMy0wMl8xMjUxMzY"});
+      expect(url.getRawParams("http://192.168.0.241:82/stocker/head.html?dir=%E5%A4%89%E6%8F%9B&file=/MjAxOS0wMy0wMl8xMjUxMzY#section")).toEqual({dir:"%E5%A4%89%E6%8F%9B", file:"/MjAxOS0wMy0wMl8xMjUxMzY"});
+      expect(url.getRawParams("http://192.168.0.241:82/stocker/head.html?dir=%E5%A4%89%E6%8F%9B&file=/MjAxOS0wMy0wMl8xMjUxMzY#")).toEqual({dir:"%E5%A4%89%E6%8F%9B", file:"/MjAxOS0wMy0wMl8xMjUxMzY"});
+      expect(url.getRawParams("http://192.168.0.241:82/stocker/head.html?dir=%E5%A4%89%E6%8F%9B")).toEqual({dir:"%E5%A4%89%E6%8F%9B"});
+      expect(url.getRawParams("http://192.168.0.241:82/stocker/head.html?dir=%E5%A4%89%E6%8F%9B&file=")).toEqual({dir:"%E5%A4%89%E6%8F%9B", file:""});
+      expect(url.getRawParams("http://192.168.0.241:82/stocker/head.html?dir=&file=")).toEqual({dir:"", file:""});
+      expect(url.getRawParams("http://192.168.0.241:82/stocker/head.html?dir=&file=#")).toEqual({dir:"", file:""});
+      expect(url.getRawParams("http://192.168.0.241:82/stocker/head.html?dir=&file=#section")).toEqual({dir:"", file:""});
+      expect(url.getRawParams("http://192.168.0.241:82/stocker/head.html")).toEqual({});
+      expect(url.getRawParams("http://192.168.0.241:82/stocker/head.html?")).toEqual({});
+      expect(url.getRawParams("http://192.168.0.241:82/stocker/head.html#")).toEqual({});
+      expect(url.getRawParams("http://192.168.0.241:82/stocker/head.html&")).toEqual({});
+    });
     test('getParams', () => {
-      expect(url.getParams("http://192.168.0.241:82/stocker/head.html?dir=%E5%A4%89%E6%8F%9B&file=/MjAxOS0wMy0wMl8xMjUxMzY")).toEqual({dir:"%E5%A4%89%E6%8F%9B", file:"/MjAxOS0wMy0wMl8xMjUxMzY"});
-      expect(url.getParams("http://192.168.0.241:82/stocker/head.html?dir=%E5%A4%89%E6%8F%9B")).toEqual({dir:"%E5%A4%89%E6%8F%9B"});
-      expect(url.getParams("http://192.168.0.241:82/stocker/head.html?dir=%E5%A4%89%E6%8F%9B&file=")).toEqual({dir:"%E5%A4%89%E6%8F%9B", file:""});
+      expect(url.getParams("http://192.168.0.241:82/stocker/head.html?dir=%E5%A4%89%E6%8F%9B&file=/MjAxOS0wMy0wMl8xMjUxMzY")).toEqual({dir:"変換", file:"/MjAxOS0wMy0wMl8xMjUxMzY"});
+      expect(url.getParams("http://192.168.0.241:82/stocker/head.html?dir=%E5%A4%89%E6%8F%9B")).toEqual({dir:"変換"});
+      expect(url.getParams("http://192.168.0.241:82/stocker/head.html?dir=%E5%A4%89%E6%8F%9B#section")).toEqual({dir:"変換"});
+      expect(url.getParams("http://192.168.0.241:82/stocker/head.html?dir=%E5%A4%89%E6%8F%9B#")).toEqual({dir:"変換"});
+      expect(url.getParams("http://192.168.0.241:82/stocker/head.html?dir=%E5%A4%89%E6%8F%9B&file=")).toEqual({dir:"変換", file:""});
       expect(url.getParams("http://192.168.0.241:82/stocker/head.html?dir=&file=")).toEqual({dir:"", file:""});
+      expect(url.getParams("http://192.168.0.241:82/stocker/head.html?dir=&file=#")).toEqual({dir:"", file:""});
+      expect(url.getParams("http://192.168.0.241:82/stocker/head.html?dir=&file=#section")).toEqual({dir:"", file:""});
       expect(url.getParams("http://192.168.0.241:82/stocker/head.html")).toEqual({});
       expect(url.getParams("http://192.168.0.241:82/stocker/head.html?")).toEqual({});
+      expect(url.getParams("http://192.168.0.241:82/stocker/head.html#")).toEqual({});
+      expect(url.getParams("http://192.168.0.241:82/stocker/head.html&")).toEqual({});
     });
   });
 
