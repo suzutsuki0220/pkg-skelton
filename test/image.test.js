f9cdb0e9913a1900f8e7acaa9fe7660fd45f3390
suzutsuki0220 <suzutsuki0220@gmail.com>
2019-03-04 09:45:27 +0000

    refactor: implement image area size calcuration

--- test/image.test.js
+++ test/image.test.js
@@ -7,10 +7,44 @@ describe('image', () => {
     image = require('../config').require.jsUtils.image;
   });
 
+  describe('isValidSize()', () => {
+    test('isValidSize', () => {
+      expect(image.isValidSize({width: 640, height: 480})).toBe(true);
+      expect(image.isValidSize({width: 0, height: 480})).toBe(false);
+      expect(image.isValidSize({width: 640, height: 0})).toBe(false);
+      expect(image.isValidSize({width: null, height: 0})).toBe(false);
+      expect(image.isValidSize({width: 640, height: null})).toBe(false);
+      expect(image.isValidSize()).toBe(false);
+    });
+  });
+
+  describe('getAreaSize()', () => {
+    test('getAreaSize', () => {
+      expect(image.getAreaSize({width: 640, height: 480})).toBe(307200);
+      expect(image.getAreaSize({width: 1, height: 1})).toBe(1);
+      expect(image.getAreaSize({width: 1, height: 0})).toBe(0);
+      expect(image.getAreaSize({width: 0, height: 1})).toBe(0);
+      expect(image.getAreaSize({width: 0, height: 0})).toBe(0);
+      expect(image.getAreaSize()).toBe(0);
+    });
+  });
+
   describe('getScale()', () => {
     test('getScale', () => {
       expect(image.getScale({width: 640, height: 480}, 1280)).toBe(2);
       expect(image.getScale({width: 640, height: 480}, 320)).toBe(0.5);
+      expect(image.getScale({width: 1, height: 1}, 320)).toBe(320);
+      expect(image.getScale({width: 1, height: 320}, 320)).toBe(1);
+      expect(image.getScale({width: 320, height: 1}, 320)).toBe(1);
+    });
+    test('getScale illegal', () => {
+      expect(image.getScale({width: 640, height: 480}, 0)).toBe(0);
+      expect(image.getScale({width: 0, height: 640}, 1280)).toBe(0);
+      expect(image.getScale({width: 640, height: 0}, 1280)).toBe(0);
+      expect(image.getScale({width: 0, height: 0}, 1280)).toBe(0);
+      expect(image.getScale({width: null, height: null}, 1280)).toBe(0);
+      expect(image.getScale({width: 640, height: 480}, null)).toBe(0);
+      expect(image.getScale({width: null, height: null}, null)).toBe(0);
     });
   });
 
