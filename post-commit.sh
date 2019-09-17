3bbec26b67d129af03410b6b863b213042e90f9a
suzutsuki0220 <suzutsuki0220@gmail.com>
2019-09-17 11:04:41 +0000

    feat: add auto tag generate by version change

---
+++ post-commit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+version=`git diff HEAD^..HEAD -- "$(git rev-parse --show-toplevel)"/package.json | grep '^\+.*version' | sed -s 's/[^0-9\.]//g'`
+
+function create_tag() {
+    git tag -a "v$1" -m "`git log -1 --format=%s`"
+    echo "Created a new tag, v$1"
+}
+
+if [ "$version" != "" ]; then
+    create_tag $version
+    exit 0
+fi
+
+exit 1
