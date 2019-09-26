69599393fe5a2f94fa5a14cab2d9d594d5932ba9
suzutsuki0220 <suzutsuki0220@gmail.com>
2019-09-26 07:04:20 +0000

    test: eslint downgrade ecmaVersion 2018 -> 2015

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
         "SharedArrayBuffer": "readonly"
     },
     "parserOptions": {
-        "ecmaVersion": 2018,
+        "ecmaVersion": 2015,
         "sourceType": "module"
     },
     "plugins": [
