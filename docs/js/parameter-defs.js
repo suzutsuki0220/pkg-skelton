9ce569f02fc771756cddaeb333b97df135464111
suzutsuki0220 <suzutsuki0220@gmail.com>
2019-12-04 03:09:55 +0000

    docs: implement library demo

--- docs/js/parameter-defs.js
+++ docs/js/parameter-defs.js
@@ -1,22 +1,41 @@
-const parameters  = {
-    character: {
-        escapeControlChar: ["code"],
-        chr: ["code"],
-        normalizeHyphen: ["string"],
-        toHalfWidthLetter: ["string"],
-        toFullWidthLetter: ["string"],
-        toFullWidthKatakana: ["string"]
-    },
-    datetime: {
-        getFormatTime: ["hour", "min", "sec", "milisec"],
-        isValidString: ["datetimeString"],
-        toUTCString: ["epoc", "addMsec"],
-        toString: ["epoc", "addMsec"],
-        toPruneString: ["epoc", "space"],
-        toRFC3339UTC: ["epoc"],
-        getDateFromDatetimeString: ["datetimeString"],
-        roundMilliEpoc: ["milli_epoc"],
-        isMatchInSeconds: ["epoc1", "epoc2"],
-        getEpoch: ["datetime"]
-    }
+let parameters  = new Object;
+
+parameters.bit = {
+    isMatch: ["value", "pattern"],
+    getMatchedOrders: ["value"]
+}
+
+parameters.character = {
+    escapeControlChar: ["code"],
+    chr: ["code"],
+    normalizeHyphen: ["string"],
+    toHalfWidthLetter: ["string"],
+    toFullWidthLetter: ["string"],
+    toFullWidthKatakana: ["string"]
+};
+
+parameters.datetime = {
+    getFormatTime: ["hour", "min", "sec", "milisec"],
+    isValidString: ["datetimeString"],
+    toUTCString: ["epoc", "addMsec"],
+    toString: ["epoc", "addMsec"],
+    toPruneString: ["epoc", "space"],
+    toRFC3339UTC: ["epoc"],
+    getDateFromDatetimeString: ["datetimeString"],
+    roundMilliEpoc: ["milli_epoc"],
+    isMatchInSeconds: ["epoc1", "epoc2"],
+    getEpoch: ["datetime"]
+};
+
+parameters.file = {
+};
+
+parameters.hex = {
+    isHex: ["value"],
+    toHex: ["decimal", "digits"],
+    toDecimal: ["hex"]
+};
+
+parameters.range = {
+    getStartEnd: ["numbersArray", "step"]
 };
